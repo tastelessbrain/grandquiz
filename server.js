892a73ab48c21247bdbc3acf4234ed4f2ce15eed
Mario Breunig <Mario-Breunig@outlook.de>
2025-09-06 09:18:27 +0200

    Bind server to all network interfaces

--- server.js
+++ server.js
@@ -162,6 +162,6 @@ app.post('/uploadMedia', upload.single('file'), (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
